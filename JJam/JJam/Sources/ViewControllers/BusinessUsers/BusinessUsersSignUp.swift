295f5e88d25ec2c0233e1c150dba55b8addd8798
segooCho <admin@adminui-MacBook-Pro.local>
2017-07-25 10:08:57 +0900

    WorkList 1. ViewController - Extension 파일 삭제

--- JJam/JJam/Sources/ViewControllers/BusinessUsers/BusinessUsersSignUp.swift
+++ JJam/JJam/Sources/ViewControllers/BusinessUsers/BusinessUsersSignUp.swift
@@ -92,4 +92,35 @@ final class BusinessUsersSignUp: UIViewController {
 }
 
 
+extension BusinessUsersSignUp: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "businessUsersSignUpTextCell", for: indexPath) as! BusinessUsersSignUpTextCell
+            cell.configure(signUp: self.signUp[0])
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "businessUsersSignUpImageCell", for: indexPath) as! BusinessUsersSignUpImageCell
+            cell.configure(signUpImageString: self.signUp[0].imageString)
+            return cell
+        }
+    }
+}
+
+extension BusinessUsersSignUp: UITableViewDelegate {
+    //cell height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return BusinessUsersSignUpTextCell.height()
+        } else {
+            return BusinessUsersSignUpImageCell.height(width: tableView.frame.width)
+        }
+    }
+}
+
+
 
