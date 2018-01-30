fa8a207e4f19cd35eae7f985e385d7a5509f0914
segooCho <admin@adminui-MacBook-Pro.local>
2018-01-30 16:07:21 +0900

    WorkList 1. image 정리

--- JJam/JJam/Sources/AppDelegate.swift
+++ JJam/JJam/Sources/AppDelegate.swift
@@ -14,9 +14,7 @@ import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-    //var destination : URL?
     
     class var instance: AppDelegate? {
         return UIApplication.shared.delegate as? AppDelegate
@@ -43,8 +41,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //BusinessUsersTabBar
         //let businessUsersTabBar = BusinessUsersTabBar(restaurant_Id: "5978502d57deb6283537150e", selectIndex: 0)
         //self.window?.rootViewController = businessUsersTabBar
-        
-        
         return true
     }
     
@@ -78,60 +74,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let navigationController = UINavigationController(rootViewController: businessUsersNotice)
         self.window?.rootViewController = navigationController
     }
-    
-    
-    //메시지 처리 예시
-    /*
-     let alertController = UIAlertController(
-     title: NSLocalizedString("Confirm", comment: "확인"),
-     message: "식당 찾기",
-     preferredStyle: .alert
-     )
-     let alertConfirm = UIAlertAction(
-     title: NSLocalizedString("Confirm", comment: "확인"),
-     style: .default) { _ in
-     // 확인 후 작업
-     }
-     alertController.addAction(alertConfirm)
-     self.present(alertController, animated: true, completion: nil)
-     */
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
